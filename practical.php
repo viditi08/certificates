560cbe335c64cec026be490b25edc22d0bf5ac71
viditi08 <50882271+viditi08@users.noreply.github.com>
2019-06-30 08:57:21 +0530

    Add files via upload

---
+++ practical.php
@@ -0,0 +1,86 @@
+<aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- User profile -->
+                
+                <!-- End User profile text-->
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <h3 class="nav-small-cap">Dashboard</h3>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" target="schoolprofile.php" aria-expanded="false"><i class="mdi mdi-school"></i><span class="hide-menu">School </span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="viewschool.php">View/Add</a></li>
+                                <li><a href="#">Add</a></li>
+                                <li><a href="#">Moderate</a></li>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="boards.php">Boards</a></li>
+                                <li><a href="facilities.php">Facilities</a></li>
+                                
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-human-male-female"></i><span class="hide-menu">Parent</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="parentview.php">View/Edit</a></li>
+                            <li><a href="#">Moderations</a></li>
+                            </ul>
+
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-human-child"></i><span class="hide-menu">Child</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="child.php">View/Edit</a></li>
+                                <li><a href="">Moderations</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="fa fa-support"></i><span class="hide-menu">Support</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="Support.php">School</a></li>
+                                <li><a href="#">Parent</a></li>
+                                <li><a href="#">Child</a></li>
+                            </ul>
+                        </li> 
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-help-circle-outline"></i><span class="hide-menu">FAQ</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="faq.php">View/Edit</a></li>
+                                <li><a href="#">Add</a></li>
+                            </ul>
+                        </li> 
+                <li>
+                            <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-human-greeting"></i><span class="hide-menu">Team Members</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="viewteammember.php">View/Edit</a></li>
+                                <li><a href="#">Add</a></li>
+                                
+                            </ul>
+                        </li>
+                         <li>
+                            <a  href="#" aria-expanded="false"><i class="mdi mdi-human-greeting"></i><span class="hide-menu">Multiple Admins</span></a>
+                            <ul aria-expanded="false" class="collapse"> 
+                            </ul>
+                        </li>
+                         <li>
+                            <a  href="#" aria-expanded="false"><i class="mdi mdi-human-greeting"></i><span class="hide-menu">Filter Modification</span></a>
+                            <ul aria-expanded="false" class="collapse"> 
+                            </ul>
+                        </li>
+                </nav>
+
+
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+            <!-- Bottom points-->
+            <div class="sidebar-footer">
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Settings"><i class="ti-settings"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Email"><i class="mdi mdi-gmail"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
+            </div>
+            <!-- End Bottom points-->
+        </aside>
