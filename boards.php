a670a8074a5da02e943b9ef7a1745ef6ffddae7e
viditi08 <50882271+viditi08@users.noreply.github.com>
2019-06-30 09:01:46 +0530

    Add files via upload

---
+++ boards.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Material Pro Admin Template - The Most Complete & Trusted Bootstrap 4 Admin Template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="css/colors/blue.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+
+    
+</style>
+
+
+</head>
+
+<body class="fix-header card-no-border">
+
+<?php include"header.php"?>
+<?php include"practical.php"?>
+
+
+
+<div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                 
+<div style="padding:20px">
+
+ <div class="card">
+    <div class="card-block">
+        <h4 class="card-title">Student Boards Information</h4>
+        <button type="button" class="btn waves-effect waves-light btn-rounded btn-danger">+Add</button>
+    
+ <table class="table table-bordered hover-table">
+  <thead>
+    <tr >
+      <th scope="col">Sr No</th>
+       
+      <th scope="col">Name</th>
+      
+     <th scope="col">Boards</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr >
+        <td>1</td>
+       
+       
+       
+        <td>viditi vartak</td>
+        <td>Maharashtra</td>
+
+
+    </tr>
+    <tr >
+        <td>2</td>
+        <td>mansi patil</td>
+        <td>Maharashtra</td>
+      
+    </tr>
+    <tr  >
+        <td>3</td>
+       
+        
+        
+        <td>mrinmayi pimple</td>
+        <td>Maharashtra</td>
+      
+    </tr>
+
+    
+  </tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <script src="../assets/plugins/jquery/jquery.min.js"></script>
+   
+    <script src="../assets/plugins/bootstrap/js/tether.min.js"></script>
+    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+ 
+    <script src="js/jquery.slimscroll.js"></script>
+
+    <script src="js/waves.js"></script>
+    
+    <script src="js/sidebarmenu.js"></script>
+   
+    <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="../assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+        <script src="js/custom.min.js"></script>
+   
+    <script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+
+
+    </body>
+    </html>
